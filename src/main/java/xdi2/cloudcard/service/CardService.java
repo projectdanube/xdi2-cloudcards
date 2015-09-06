b376a9ae9644783d650732494bb8fa632615df45
peacekeeper <markus@projectdanube.org>
2015-09-06 18:07:20 +0400

    updated API

--- src/main/java/xdi2/cloudcard/service/CardService.java
+++ src/main/java/xdi2/cloudcard/service/CardService.java
@@ -33,6 +33,7 @@ import xdi2.discovery.XDIDiscoveryClient;
 import xdi2.discovery.XDIDiscoveryResult;
 import xdi2.messaging.Message;
 import xdi2.messaging.MessageEnvelope;
+import xdi2.messaging.constants.XDIMessagingConstants;
 import xdi2.messaging.operations.GetOperation;
 import xdi2.messaging.response.MessagingResponse;
 
@@ -41,112 +42,112 @@ public class CardService {
 
 	private static final Logger log = LoggerFactory.getLogger(CardService.class);
 
-	
+
 	@Autowired
 	private CardXdiModelConverter cardXdiModelConverter;
 
 
 	public Card getCard(String id) throws Xdi2ClientException {
-		
+
 		log.debug("Trying to get card in address: " + id);
-		
+
 		// handles the shortcut address
 		if (id.contains("$card") == false) {
 			id = id + "$card";
 		}
-		
+
 		id += "$public";
-		
+
 		Card card = getCardFromPublicLC(id);
 
 		return card;
 	}
-	
+
 
 	public Card getCard(String id, String privateCardXdi) throws Xdi2ParseException, IOException {
 
 		Graph cardGraph = MemoryGraphFactory.getInstance().openGraph();
 		XDIReader xdiReader = XDIReaderRegistry.getAuto();
 		xdiReader.read(cardGraph, new StringReader(privateCardXdi));
-		
+
 		ContextNode cardContextNode = cardGraph.getDeepContextNode(XDIAddress.create(id));
 		if (cardContextNode == null) {
 			return null;
 		}
-		
+
 		Card card = cardXdiModelConverter.convertXdiToCard(cardContextNode);
-		
-		
+
+
 		// Discover CloudName
 		ReadOnlyIterator<Relation> relations = cardGraph.getDeepRelations(XDIAddress.create(getCloudIdentifier(id)), XDIAddress.create("$is$ref"));
 		while (relations.hasNext()) {
 			Relation r = relations.next();
 			card.setCloudName(r.getTargetXDIAddress().toString());
 		}
-		
+
 		return card;
 	}
-	
+
 	private Card getCardFromPublicLC(String cardXdiAddress) throws Xdi2DiscoveryException, Xdi2ClientException {
 
 		XDIAddress cloudIdentifier = XDIAddress.create(getCloudIdentifier(cardXdiAddress));
-		
+
 		// Discover Cloud Endpoint
 		XDI2X509TrustManager.enable();
-		
-		XDIDiscoveryClient xdiDiscoveryClient = XDIDiscoveryClient.XDI2_DISCOVERY_CLIENT;
-        XDIDiscoveryResult result = xdiDiscoveryClient.discoverFromRegistry(cloudIdentifier);
 
-        if (result.getCloudNumber() == null) {
-    		throw new RuntimeException("It was not possible to discover " + cloudIdentifier);
-        }
-        
-        URI xdiEndpoint = result.getXdiEndpointUri();
-        CloudNumber cloudNumber = result.getCloudNumber();
-        
-        // Query cloud for cloud names
+		XDIDiscoveryClient xdiDiscoveryClient = XDIDiscoveryClient.XDI2_DISCOVERY_CLIENT;
+		XDIDiscoveryResult result = xdiDiscoveryClient.discoverFromRegistry(cloudIdentifier);
+
+		if (result.getCloudNumber() == null) {
+			throw new RuntimeException("It was not possible to discover " + cloudIdentifier);
+		}
+
+		URI xdiEndpoint = result.getXdiEndpointUri();
+		CloudNumber cloudNumber = result.getCloudNumber();
+
+		// Query cloud for cloud names
 		XDIClient client = new XDIHttpClient(xdiEndpoint);
 		MessageEnvelope me = new MessageEnvelope();
 		Message m = me.createMessage(XDIAddress.create("$anon"));
 		m.setToPeerRootXDIArc(cloudNumber.getPeerRootXDIArc());
 		m.setLinkContractClass(PublicLinkContract.class);
-		
+
 		GetOperation operation = m.createGetOperation(XDIAddress.create(cardXdiAddress));
-		operation.setParameter(GetOperation.XDI_ADD_PARAMETER_DEREF, Boolean.TRUE);
-		
+		operation.setParameter(XDIMessagingConstants.XDI_ADD_OPERATION_PARAMETER_DEREF, Boolean.TRUE);
+
 		m.createGetOperation(XDIStatementUtil.concatXDIStatement(cloudNumber.getXDIAddress(), XDIStatement.create("/$is$ref/{}")));
 
 		MessagingResponse mr = client.send(me);
-		
+
 		ContextNode cardContextNode = mr.getResultGraph().getRootContextNode().getDeepContextNode(XDIAddress.create(cardXdiAddress));
 		if (cardContextNode == null) {
 			throw new RuntimeException("Card not found.");
 		}
-		
+
 		Card card = cardXdiModelConverter.convertXdiToCard(cardContextNode);
-		
-		
+
+
 		// Discover CloudName
 		ReadOnlyIterator<Relation> relations = mr.getResultGraph().getDeepRelations(cloudNumber.getXDIAddress(), XDIAddress.create("$is$ref"));
 		while (relations.hasNext()) {
 			Relation r = relations.next();
 			card.setCloudName(r.getTargetXDIAddress().toString());
 		}
-		
+
 		return card;
-		
-		
+
+
 	}
-		
+
 	private static String getCloudIdentifier(String cardXdiAddress) {
-	
+
 		/* 
 		 * Possible formats:
 		 * 				* =alice[$card]!:uuid:1111
 		 * 				* =alice#home$card
 		 * 				* =alice$card
-		*/
-		
+		 */
+
 		int end = cardXdiAddress.indexOf("#");
 		if (end < 0)
 			end = cardXdiAddress.indexOf("[$");
@@ -154,9 +155,9 @@ public class CardService {
 			end = cardXdiAddress.indexOf("$");
 		if (end < 0)
 			return null;
-		
+
 		return cardXdiAddress.substring(0, end);
 	}
-	
+
 
 }
